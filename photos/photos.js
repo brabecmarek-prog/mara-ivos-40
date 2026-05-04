890917f6d4c1d6a621a8133882417796e9db3331
Marek Brabec <MBrabec@moravia.com>
2026-05-04 18:35:32 +0200

    Add birthday party webpage

---
+++ photos/photos.js
@@ -0,0 +1,38 @@
+window.partyPhotoGallery = {
+  // Change this to another folder or full URL later, for example:
+  // basePath: "https://example.com/party-photos/",
+  basePath: "photos/",
+
+  // Photos shown in the top collage. One random frame changes every 3 seconds.
+  // If this stays empty, the page will reuse random photos from "photos".
+  heroPhotos: [
+    "Screenshot 2026-05-04 122340.png",
+    "Screenshot 2026-05-04 141316.png",
+    "Screenshot 2026-05-04 153106.png",
+    "Screenshot 2026-05-04 153608.png"
+  ],
+
+  // Add filenames from basePath, or objects with src, alt, and caption.
+  photos: [
+    {
+      src: "Screenshot 2026-05-04 122340.png",
+      alt: "Fotka k oslave",
+      caption: "Vzpominka"
+    },
+    {
+      src: "Screenshot 2026-05-04 141316.png",
+      alt: "Fotka k oslave",
+      caption: "Vzpominka"
+    },
+    {
+      src: "Screenshot 2026-05-04 153106.png",
+      alt: "Fotka k oslave",
+      caption: "Vzpominka"
+    },
+    {
+      src: "Screenshot 2026-05-04 153608.png",
+      alt: "Fotka k oslave",
+      caption: "Vzpominka"
+    }
+  ]
+};
