f9a2f1d3d122b71138ea8fac21b815d1b0f78572
Marek Brabec <MBrabec@moravia.com>
2026-05-05 14:14:44 +0200

    Replace hero frames with photo carousel

--- photos/photos.js
+++ photos/photos.js
@@ -1,38 +1,35 @@
 window.partyPhotoGallery = {
-  // Change this to another folder or full URL later, for example:
-  // basePath: "https://example.com/party-photos/",
   basePath: "photos/",
 
-  // Photos shown in the top collage. One random frame changes every 3 seconds.
-  // If this stays empty, the page will reuse random photos from "photos".
+  // Photos shown in the top carousel. The strip moves by 3 photos every 3 seconds.
   heroPhotos: [
+    "Screenshot 2026-04-20 111050.png",
+    "Screenshot 2026-04-20 111257.png",
+    "Screenshot 2026-04-20 112005.png",
+    "Screenshot 2026-04-20 122007.png",
+    "Screenshot 2026-04-20 123436.png",
+    "Screenshot 2026-04-20 145117.png",
+    "Screenshot 2026-04-20 160820.png",
+    "Screenshot 2026-04-21 102825.png",
+    "Screenshot 2026-04-21 102920.png",
+    "Screenshot 2026-04-21 103005.png",
+    "Screenshot 2026-04-21 133130.png",
+    "Screenshot 2026-04-21 141228.png",
+    "Screenshot 2026-04-21 141859.png",
+    "Screenshot 2026-04-21 184227.png",
+    "Screenshot 2026-04-21 184655.png",
+    "Screenshot 2026-04-21 185601.png",
+    "Screenshot 2026-04-21 185935.png",
+    "Screenshot 2026-04-21 195018.png",
+    "Screenshot 2026-04-21 195813.png",
+    "Screenshot 2026-04-22 101318.png",
+    "Screenshot 2026-04-22 101414.png",
+    "Screenshot 2026-04-22 125528.png",
     "Screenshot 2026-05-04 122340.png",
     "Screenshot 2026-05-04 141316.png",
     "Screenshot 2026-05-04 153106.png",
     "Screenshot 2026-05-04 153608.png"
   ],
 
-  // Add filenames from basePath, or objects with src, alt, and caption.
-  photos: [
-    {
-      src: "Screenshot 2026-05-04 122340.png",
-      alt: "Fotka k oslave",
-      caption: "Vzpominka"
-    },
-    {
-      src: "Screenshot 2026-05-04 141316.png",
-      alt: "Fotka k oslave",
-      caption: "Vzpominka"
-    },
-    {
-      src: "Screenshot 2026-05-04 153106.png",
-      alt: "Fotka k oslave",
-      caption: "Vzpominka"
-    },
-    {
-      src: "Screenshot 2026-05-04 153608.png",
-      alt: "Fotka k oslave",
-      caption: "Vzpominka"
-    }
-  ]
+  photos: []
 };
