3825745a4818846880dda7b2f7f112427cb60561
Marek Brabec <MBrabec@moravia.com>
2026-05-06 14:25:29 +0200

    Configure shared questions endpoint

--- questions/config.js
+++ questions/config.js
@@ -1,3 +1,3 @@
 // Paste the deployed Google Apps Script Web App URL here.
 // Until this is filled in, questions are saved only in the current browser.
-window.partyQuestionsEndpoint = "";
+window.partyQuestionsEndpoint = "https://script.google.com/macros/s/AKfycbyigLmwRexPzwvgH5q1DiMDcGa8s3LcL_zZiulvJdgXwqpPY__CjIbS3sCiIX3XDL-_/exec";
