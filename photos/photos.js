80de99c1942169cc55f5af258f9c157fa51bc116
Marek Brabec <MBrabec@moravia.com>
2026-06-03 20:23:52 +0200

    Update hero gallery photos

--- photos/photos.js
+++ photos/photos.js
@@ -1,34 +1,23 @@
 window.partyPhotoGallery = {
   basePath: "photos/",
 
-  // Photos shown in the top carousel. The strip moves by 3 photos every 3 seconds.
+  // Photos shown in the top carousel.
   heroPhotos: [
-    "Screenshot 2026-04-20 111050.png",
-    "Screenshot 2026-04-20 111257.png",
-    "Screenshot 2026-04-20 112005.png",
-    "Screenshot 2026-04-20 122007.png",
-    "Screenshot 2026-04-20 123436.png",
-    "Screenshot 2026-04-20 145117.png",
-    "Screenshot 2026-04-20 160820.png",
-    "Screenshot 2026-04-21 102825.png",
-    "Screenshot 2026-04-21 102920.png",
-    "Screenshot 2026-04-21 103005.png",
-    "Screenshot 2026-04-21 133130.png",
-    "Screenshot 2026-04-21 141228.png",
-    "Screenshot 2026-04-21 141859.png",
-    "Screenshot 2026-04-21 184227.png",
-    "Screenshot 2026-04-21 184655.png",
-    "Screenshot 2026-04-21 185601.png",
-    "Screenshot 2026-04-21 185935.png",
-    "Screenshot 2026-04-21 195018.png",
-    "Screenshot 2026-04-21 195813.png",
-    "Screenshot 2026-04-22 101318.png",
-    "Screenshot 2026-04-22 101414.png",
-    "Screenshot 2026-04-22 125528.png",
-    "Screenshot 2026-05-04 122340.png",
-    "Screenshot 2026-05-04 141316.png",
-    "Screenshot 2026-05-04 153106.png",
-    "Screenshot 2026-05-04 153608.png"
+    "37021332_10212899483385311_997360649593946112_n.jpg",
+    "506052825_10230563164446298_2501836351600908462_n.jpg",
+    "DSCF0075.JPG",
+    "IMG_0186.JPG",
+    "IMG_0479.JPG",
+    "IMG_1315.JPG",
+    "IMG_2635.JPG",
+    "IMG_4149.JPEG",
+    "IMG_4288.JPG",
+    "IMG_7415.JPG",
+    "IMG_8664.JPG",
+    "IMG_8700.JPEG",
+    "IMG_9442.JPEG",
+    "IMG_9591.JPG",
+    "P1060347.JPG"
   ],
 
   photos: []
